98873a246629f54b63a7c81489ec91de79eb2e59
jmagdeska <janamagdeska11@gmail.com>
2017-02-13 21:05:07 +0100

    Morphology added

--- blur_thresh_contour.py
+++ blur_thresh_contour.py
@@ -8,13 +8,17 @@ letters = ["a", "b", "f", "i", "l", "m", "o", "t", "u", "z"]
 
 for i in range(10):
     for j in range(1,10):
-        single = cv2.imread('C:\Users\Jana\PyCharmProjects\skin-detection/frames_skin/' +letters[i] + format(j) + '.png')
-        grey1 = cv2.cvtColor(single, cv2.COLOR_BGR2GRAY)
+        single = cv2.imread('C:\Users\Jana\PyCharmProjects\skin-detection/frames_skin/' +letters[i] + "-1-" + format(j) + '.png')
+        kernel = np.ones((5, 5), np.uint8)
+        mor = cv2.morphologyEx(single, cv2.MORPH_OPEN, kernel)
+        mor2 = cv2.dilate(mor, kernel, iterations=1)
+        mor3 = cv2.morphologyEx(mor2, cv2.MORPH_CLOSE, kernel)
+        grey1 = cv2.cvtColor(mor3, cv2.COLOR_BGR2GRAY)
         blurred1 = cv2.GaussianBlur(grey1, (5, 5), 0)
         _, thresh1 = cv2.threshold(blurred1, 127, 255,
                                    cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        cv2.imwrite("frames_thresh/" + letters[i] + format(j) + ".png", thresh1)
+        cv2.imwrite("frames_thresh/" + letters[i] + "-1-" + format(j) + ".png", thresh1)
 
         cnts = cv2.findContours(thresh1.copy(), cv2.RETR_EXTERNAL,
                                 cv2.CHAIN_APPROX_SIMPLE)
@@ -25,7 +29,7 @@ for i in range(10):
             # compute the center of the contour
             M = cv2.moments(c)
 
-            if M["m00"] > 2000:
+            if M["m00"] > 4000:
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
 
@@ -41,10 +45,12 @@ for i in range(10):
                 if(h%2 != 0): offset_h = 1
                 if(w%2 != 0): offset_w = 1
 
+                # print format(h_max/2) + " " + format(h/2) + " " + format(w_max/2) + " " + format(w/2)
+
                 black[h_max/2 - h/2 - offset_h: h_max/2 + h/2, w_max/2 - w/2 - offset_w: w_max/2 + w/2] = thresh1[y:y+h, x:x+w]
                 # cv2.imshow("Cropped gesture",thresh1[y:y+h, x:x+w])
                 # cv2.waitKey(0)
-                cv2.imwrite("frames_thresh_center/" + letters[i] + format(j) + ".png", black)
+                cv2.imwrite("frames_thresh_center/" + letters[i] + "-1-" + format(j) + ".png", black)
 
             else:
                 cX, cY = 0, 0
--- detect_skin_video.py
+++ detect_skin_video.py
@@ -9,34 +9,42 @@ def draw_circle(event,x,y,flags,param):
     global mouseX,mouseY, color
     if event == cv2.EVENT_LBUTTONDBLCLK:
         mouseX,mouseY = x,y
-        image = cv2.imread("frames_initials/initial_" + letters[l] + ".png")
+        image = cv2.imread("frames_initials/initial_" + letters[l] + "-1.png")
         initial = imutils.resize(image,width=400, height=300)
         color = initial[y, x]
         print("My color is " + format(color))
-
+    if event == cv2.EVENT_RBUTTONDBLCLK:
+        mouseX,mouseY = x,y
+        cv2.imwrite("screen2.png", frame)
+        image = cv2.imread("screen2.png")
+        image = imutils.resize(image, width=400)
+        color_clicked = image[y, x]
+        print("Color clicked is " + format(color_clicked))
+        if color_clicked[2] < (color[2] + 40) and color_clicked[2] > (color[2] - 40) and color_clicked[1] < (color[1] + 50) and color_clicked[1] > (color[1] - 50) and color_clicked[0] < (color[0] + 50) and color_clicked[0] > (color[0] - 50):
+            print("Skin detected")
 
 for l in range(10):
     print("Extracting initial frame of letter " + letters[l])
-    cap = cv2.VideoCapture('C:\Users\Jana\PyCharmProjects\skin-detection/videos/gesture-' + letters[l] + '.avi')
+    cap = cv2.VideoCapture('C:\Users\Jana\PyCharmProjects\skin-detection/videos/gesture-' + letters[l] + '-1.avi')
     while not cap.isOpened():
-        cap = cv2.VideoCapture('C:\Users\Jana\PyCharmProjects\skin-detection/videos/gesture-' + letters[l] + '.avi')
+        cap = cv2.VideoCapture('C:\Users\Jana\PyCharmProjects\skin-detection/videos/gesture-' + letters[l] + '-1.avi')
         cv2.waitKey(1000)
         print "Wait for the header"
     k = 1
 
-    cap.set(1, 5)
+    cap.set(1, 2)
     ret, frame = cap.read()
-    cv2.imwrite("frames_initials/initial_" + letters[l] + ".png", frame)
+    cv2.imwrite("frames_initials/initial_" + letters[l] + "-1.png", frame)
 
-    for i in range(5,75,7):
+    for i in range(2,75,3):
         cap.set(1, i)
         ret, frame = cap.read()
-        cv2.imwrite("frames/initial_" + letters[l] + format(k) + ".png", frame)
+        cv2.imwrite("frames/initial_" + letters[l] + "-1-" + format(k) + ".png", frame)
         k += 1
 
 for l in range(10):
     print("Select skin color initial frame of letter " + letters[l])
-    image_initial = cv2.imread("frames_initials/initial_" + letters[l] + ".png")
+    image_initial = cv2.imread("frames_initials/initial_" + letters[l] + "-1.png")
     initial = imutils.resize(image_initial,width=400, height=225)
     cv2.namedWindow('images')
     cv2.setMouseCallback('images', draw_circle)
@@ -45,16 +53,15 @@ for l in range(10):
     cv2.destroyAllWindows()
 
     print("Separate hand from each frame of letter " + letters[l])
-    for i in range(1,10):
-        a = cv2.imread("frames/initial_" + letters[l] + format(i) + ".png")
+    for i in range(1,25):
+        a = cv2.imread("frames/initial_" + letters[l] + "-1-" + format(i) + ".png")
         initial = imutils.resize(a,width=400, height=225)
         blank_image = np.zeros((300,400,3), np.uint8)
         for a in range(225):
             for b in range(400):
                 color_clicked = initial[a,b]
-                if color_clicked[2] < (color[2] + 40) and color_clicked[2] > (color[2] - 40) and color_clicked[1] < (color[1] + 60) and color_clicked[1] > (color[1] - 60) and color_clicked[0] < (color[0] + 80) and color_clicked[0] > (color[0] - 80) and color_clicked[0] < 230 and color_clicked[1] < 240 and color_clicked[2] > 210:
+                if color_clicked[2] < (color[2] + 40) and color_clicked[2] > (color[2] - 40) and color_clicked[1] < (color[1] + 60) and color_clicked[1] > (color[1] - 60) and color_clicked[0] < (color[0] + 80) and color_clicked[0] > (color[0] - 80) :
                     blank_image[a,b] = initial[a,b]
-        cv2.imwrite("frames_skin/" + letters[l] + format(i) + ".png", blank_image)
-
+        cv2.imwrite("frames_skin/" + letters[l] + "-1-" + format(i) + ".png", blank_image)
 
 print("Done with processing")
