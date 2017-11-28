7da9d31bc581451076484da363b8bd37ea7fac47
Jana <janamagdeska11@gmail.com>
2017-11-28 12:48:17 +0100

    HOG added.

--- keras_theano.py
+++ keras_theano.py
@@ -2,7 +2,10 @@ from sklearn.svm import SVC
 import cv2
 import numpy as np
 
-dir_center = "frames_thresh_center/"
+SZ=20
+bin_n = 16 # Number of bins
+
+dir_center = "frames_skin/"
 
 letters = ["a", "b", "v", "g", "d", "gj", "e", "zh", "z", "dj", "i", "j", "k", "l", "lj", "m", "n", "nj", "o", "p", "r", "s", "t", "kj", "u", "f", "h", "c", "ch", "dz", "sh"]
 
@@ -25,6 +28,26 @@ class SVM(StatModel):
 
     def predict(self, samples):
         return self.model.predict(samples)[1].ravel()
+
+
+def deskew(img):
+    m = cv2.moments(img)
+    if abs(m['mu02']) < 1e-2:
+        return img.copy()
+    skew = m['mu11']/m['mu02']
+    M = np.float32([[1, skew, -0.5*SZ*skew], [0, 1, 0]])
+    img = cv2.warpAffine(img,M,(SZ, SZ),flags=affine_flags)
+    return img
+def hog(img):
+    gx = cv2.Sobel(img, cv2.CV_32F, 1, 0)
+    gy = cv2.Sobel(img, cv2.CV_32F, 0, 1)
+    mag, ang = cv2.cartToPolar(gx, gy)
+    bins = np.int32(bin_n*ang/(2*np.pi))    # quantizing binvalues in (0...16)
+    bin_cells = bins[:10,:10], bins[10:,:10], bins[:10,10:], bins[10:,10:]
+    mag_cells = mag[:10,:10], mag[10:,:10], mag[:10,10:], mag[10:,10:]
+    hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
+    hist = np.hstack(hists)     # hist is a 64 bit vector
+    return hist
 
 w_max=300
 h_max=400
@@ -69,12 +92,12 @@ for d in data_train1:
 
 for d in data_train2:
     train1.append(d)
-#
-# for d in data_train3:
-#     train1.append(d)
-#
-# for d in data_train4:
-#     train1.append(d)
+
+for d in data_train3:
+    train1.append(d)
+
+for d in data_train4:
+    train1.append(d)
 
 training_set = []
 testing_set = []
@@ -84,7 +107,7 @@ training_y = [] #-klasite odnosno bukite
 
 ######### dodeli trening Y
 
-for c in range(2):
+for c in range(4):
     for i in range(31):
         for j in range(52): # od sekoja bukva 4 primeroka od 13 dataseta
             training_y.append(i)
@@ -97,25 +120,28 @@ for j in range(31):
     for k in range(52):  # od sekoja bukva 4 primeroka od 13 dataseta
         cross_test_y.append(j)
 
+
 for file in train1:
     print("File: " + file)
     img = cv2.imread(dir_center + file, 0)
-    xarr = np.squeeze(np.array(img).astype(np.float32))
-    m, v = cv2.PCACompute(xarr, mean=None)
-    arr = np.array(v)
-    flat_arr = arr.ravel()
-    training_set.append(flat_arr)
+    hist = hog(img)
+    # xarr = np.squeeze(np.array(img).astype(np.float32))
+    # m, v = cv2.PCACompute(xarr, mean=None)
+    # arr = np.array(v)
+    # flat_arr = arr.ravel()
+    training_set.append(hist)
 
 for file in data_train5:
     print("File test: " + file)
     img = cv2.imread(dir_center + file, 0)
-    xarr = np.squeeze(np.array(img).astype(np.float32))
-    m, v = cv2.PCACompute(xarr, mean=None)
-    arr = np.array(v)
-    flat_arr = arr.ravel()
-    testing_set.append(flat_arr)
+    hist = hog(img)
+    # xarr = np.squeeze(np.array(img).astype(np.float32))
+    # m, v = cv2.PCACompute(xarr, mean=None)
+    # arr = np.array(v)
+    # flat_arr = arr.ravel()
+    testing_set.append(hist)
 
-print len(training_set), len(training_y)
+print testing_set
 
 trainData = np.float32(training_set)
 responses = np.array(training_y)
@@ -124,7 +150,7 @@ print "Done with new_listing"
 
 crossTrain = np.float32(trainData)
 
-model = SVM(C=10, gamma=0.018)
+model = SVM(C=2.67, gamma=5.383)
 model.train(trainData, np.array(training_y))
 
 #
